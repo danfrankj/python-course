af36f2d33846e0c6828dc2014fb588da996945ae
Austin Benson <arbenson@berkeley.edu>
2013-01-04 15:28:32 -0800

    stocks question for homework3

---
+++ part-3/homework-3/code/stocks.py
@@ -0,0 +1,53 @@
+class Portfolio():
+  def __init__(self):
+    self.stocks = {}
+
+  """
+   Add the number of shares to the stock in this portfolio.
+   At this moment, price_per_share is the price of the stock,
+   so update the price per share of the stock in your Portfolio
+   if you already own the stock
+  """
+  def add_stock(self, symbol, price_per_share, shares):
+    # IMPLEMENT
+    return
+
+  """
+   Return the symbol of the stock in which this portfolio
+   is the most invested.  In other words, return the symbol
+   of the stock for which the price per share multiplied by the
+   number of shares is the most.
+  """
+  def most_money(self):
+    # IMPLEMENT
+    return ''
+
+  """
+   Implement __contains__ so that it returns True if the stock symbol
+   (provided by key) is in this portfolio.
+  """
+  def __contains__(self, key):
+    # IMPLEMENT
+    return False
+
+techs = Portfolio()
+
+def test1():
+  techs.add_stock('APPL', 600.41, 0.3)
+  techs.add_stock('GOOG', 630.29, 1.2)
+  techs.add_stock('FB', 19.56, 30)
+  print techs.most_money()
+  # Facebook just skyrocketed!
+  techs.add_stock('FB', 45.73, 2)
+  print techs.most_money()
+
+test1()
+
+def test2():
+  if 'P' not in techs:
+    print 'Maybe I should buy Pandora stock'
+    techs.add_stock('P', 18.73, 70)
+  if 'P' in techs:
+    print 'I now have Pandora stock'
+
+test2()
