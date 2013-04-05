334b92d7e9d19f40b10977158eb3ebf5c84cded1
Austin Benson <arbenson@gmail.com>
2013-04-05 16:51:20 -0700

    20-digit approximations do not make sense in floating point with 16 decimal digits of accuracy

--- part-2/exercises-2/code/kludge2.py
+++ part-2/exercises-2/code/kludge2.py
@@ -1,8 +1,8 @@
 import math
-# first 20 k-digit approximations of pi
+# first 14 k-digit approximations of pi
 approximations = []
 i = 1
-while i <= 20:
+while i <= 14:
   approx = round(math.pi, i)
   approximations.append(approx)
   i += 1
